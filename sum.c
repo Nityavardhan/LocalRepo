fb3ef53776156641f59bcac069dfb97e7e51137c
Nityavardhan SIngh <lightningnsh17@gmail.com>
2023-12-29 22:51:55 +0530

    Adding some new files

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter the first number:\n");
+    scanf("%d", &a);
+    printf("Enter the second number:\n");
+    scanf("%d", &b);
+
+    sum = a + b;
+    printf("The sum of both the numbers is %d", sum );
+        return 0;
+}
